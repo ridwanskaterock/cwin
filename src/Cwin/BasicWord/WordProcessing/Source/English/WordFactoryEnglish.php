8ff63ab83f05a7f4f7ad3fe8b27098caa11a9ab1
ridwan <ridwanskaterocks@gmail.com>
2015-11-24 09:55:16 +0700

    Add word factory english

--- src/Cwin/BasicWord/WordProcessing/Source/English/WordFactoryEnglish.php
+++ src/Cwin/BasicWord/WordProcessing/Source/English/WordFactoryEnglish.php
@@ -1,14 +1,14 @@
 <?php 
 
-namespace Cwin\BasicWord\WordProcessing\Source\Indonesia;
+namespace Cwin\BasicWord\WordProcessing\Source\English;
 
 use Cwin\BasicWord\WordProcessing\Source\WordFactoryInterface;
 
-class WordFactoryIndonesia implements WordFactoryInterface
+class WordFactoryEnglish implements WordFactoryInterface
 {
 	public function sourceBaseWord()
 	{
-		$baseWordPath = __DIR__ . '/Data/Dictionary-indonesia.txt';
+		$baseWordPath = __DIR__ . '/Data/Dictionary-english.txt';
 		$basicWordContents = file_get_contents($baseWordPath);
 
 		return $basicWordContents;
